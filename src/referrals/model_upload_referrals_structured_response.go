33e31b8d52e78fc0f61aece50788bc1aeafbe132
bos-hieu <12520839@gm.uit.edu.vn>
2023-04-13 18:22:46 +0700

    add referrals

---
+++ src/referrals/model_upload_referrals_structured_response.go
@@ -0,0 +1,8 @@
+package referrals
+
+type UploadReferralsStructuredResponse struct {
+    ReferralServiceResult struct {
+        Success bool `json:"success"`
+    } `json:"referralServiceResult"`
+    SkippedReferrals []interface{} `json:"skippedReferrals"`
+}
